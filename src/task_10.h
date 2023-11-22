fd6bb0df298d16bb1e1dddf8e5c137e19ca53992
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2023-11-22 23:22:28 +0500

    Create task_10.h

---
+++ src/task_10.h
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+string permuteString(const string& s) {
+    string t = s;
+    vector<pair<int, char>> vowelIndices;
+
+    // Find vowel indices and their corresponding characters in s
+    for (int i = 0; i < s.length(); i++) {
+        char c = tolower(s[i]);
+        if (c == 'a'  c == 'e'  c == 'i'  c == 'o'  c == 'u')
+            vowelIndices.push_back(make_pair(i, s[i]));
+    }
+
+    // Sort the vowel indices based on the ASCII values of the corresponding characters
+    sort(vowelIndices.begin(), vowelIndices.end(),
+              [](const pair<int, char>& a, const pair<int, char>& b) {
+                  return tolower(a.second) < tolower(b.second);
+              });
+
+    // Reconstruct the resulting string t by placing the sorted vowels back into their original positions
+    for (const auto& pair : vowelIndices)
+        t[pair.first] = pair.second;
+
+    return t;
+}
+
+int main() {
+    string s;
+    cin >> s;
+
+    string t = permuteString(s);
+    cout << "Resulting String: " << t << endl;
+
+    return 0;
+}
