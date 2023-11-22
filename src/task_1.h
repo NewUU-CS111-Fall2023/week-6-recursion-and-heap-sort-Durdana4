8a23c41757811e2c4e42e77636b008083538f6f4
Durdana4 <127374217+Durdana4@users.noreply.github.com>
2023-11-22 20:47:17 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -3,3 +3,36 @@
  * Date:
  * Name:
  */
+#include <iostream>
+using namespace std;
+void printNumbers(int A, int B) {
+    // Base case: A and B are equal
+    if (A == B) {
+        cout << A << endl;
+        return;
+    }
+
+    // Recursive case: A < B
+    if (A < B) {
+        cout << A << endl;
+        printNumbers(A + 1, B);
+    }
+    // Recursive case: A > B
+    else {
+        cout << A << endl;
+        printNumbers(A - 1, B);
+    }
+}
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+
+    if (A < B) {
+        printNumbers(A, B);
+    } else {
+        printNumbers(A, B);
+    }
+
+    return 0;
+}
